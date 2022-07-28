579985cb0bc1c633e06999bcb2f60b3825cf568e
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-27 23:14:06 -0700

    added doc for constructor

--- src/Date.java
+++ src/Date.java
@@ -2,6 +2,13 @@ public class Date {
    private int year;
    private int month;
    private int day;
+
+    /**
+     * Full constructor for Date class
+     * @param year  year
+     * @param month month of the year
+     * @param day   day of the month
+     */
    public Date (int year, int month, int day){
        this.year = year;
        this.month = month;
