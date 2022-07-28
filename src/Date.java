69aaa397dcbcd6324ae2f47eef07590a6371b97f
obaidsi <52484343+obaidsi@users.noreply.github.com>
2022-07-27 23:28:55 -0700

    added documentation for Date.java

--- src/Date.java
+++ src/Date.java
@@ -14,10 +14,40 @@ public class Date {
        this.month = month;
        this.day = day;
    }
+
+    /**
+     * Retrieves the year
+     * @return  year
+     */
    public int getYear(){ return year; }
+
+    /**
+     * Retrieve the month
+     * @return  month of the year
+     */
    public int getMonth(){ return month; }
+
+    /**
+     * Retrieve the day
+     * @return  day of the month
+     */
    public int getDay(){ return day; }
+
+    /**
+     * Update year
+     * @param year year
+     */
    public void setYear(int year){ this.year = year; }
+
+    /**
+     * Update month of the year
+     * @param month month of the year
+     */
    public void setMonth(int month){ this.month = month;}
+
+    /**
+     * Updage day of the month
+     * @param day   day of the month
+     */
    public void setDay(int day){ this.day = day; }
 }
